352971cecc2b6de64ea6447f00f3d996f88944b6
Hamzagb05 <chatir85@gmail.com>
2025-04-07 08:58:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,16 +6,17 @@ app = Flask(__name__)
 def exercice(valeur):
     pyramide = ''
     for i in range(1, valeur + 1):
-    
-        left_side = ''.join(str(x) for x in range(1, i + 1))
-        
-        right_side = ''.join(str(x) for x in range(i - 1, 0, -1))
        
+        left_side = ''.join(str(x) for x in range(1, i + 1))
+       
+        right_side = ''.join(str(x) for x in range(i - 1, 0, -1))
+        
         ligne = left_side + right_side
         
         pyramide += ' ' * (valeur - i) + ligne + '<br>'
-    return pyramide  
+    return pyramide 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
