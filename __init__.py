a59f6904fb9abf4f245c05d69f4a7a7cebd3d195
Hamzagb05 <chatir85@gmail.com>
2025-04-07 09:05:58 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,21 +1,29 @@
 from flask import Flask
+from flask import render_template
+from flask import json
 
 app = Flask(__name__)
 
 @app.route('/<int:valeur>')
-def exercice(valeur):
-    pyramide = ''
-    for i in range(1, valeur - 1):
-       
-        left_side = ''.join(str(x) for x in range(1, i + 1))
-       
-        right_side = ''.join(str(x) for x in range(i - 1, 0, -1))
+def pyramide(valeur):
+    resultat = '<pre>'
+    
+    for i in range(1, valeur + 1):
+        # Créer la partie avant le sommet
+        gauche = ''.join(str(x) for x in range(1, i + 1))
+        # Créer la partie après le sommet
+        droite = ''.join(str(x) for x in range(i - 1, 0, -1))
         
-        ligne = left_side + right_side
+        # Ajouter les espaces nécessaires pour centrer
+        espaces = ' ' * (valeur - i)
         
-        pyramide += ' ' * (valeur - i) + ligne + '<br>'
-    return pyramide 
+        # Combiner tout cela
+        resultat += espaces + gauche + droite + '\n'
+    
+    resultat += '</pre>'
+    return resultat
 
+ 
 if __name__ == "__main__":
     app.run(debug=True)
 
