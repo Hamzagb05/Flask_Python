b0fb8639ca6de79df3e955e846412cbd991db0b0
Hamzagb05 <chatir85@gmail.com>
2025-04-07 10:11:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,19 +4,29 @@ app = Flask(__name__)
 
 @app.route('/<int:n>')
 def fibonacci(n):
-    # Initialiser les deux premiers termes
-    a , b = 0, 1
+    # Initialiser la variable avec le premier terme
+    U = 0
     suite = []
 
-    # Générer la suite jusqu'au terme n
-    for _ in range(n):
-        suite.append(a)
-        a, b = b, a + b  # Mise à jour des valeurs
+    # Utiliser une seule variable pour générer la suite
+    for i in range(n):
+        if i == 0:
+            suite.append(U)  # Ajouter le premier terme
+        elif i == 1:
+            U = 1  # Deuxième terme
+            suite.append(U)
+        else:
+            # Calculer le terme suivant en utilisant la variable U
+            U = suite[i - 1] + suite[i - 2]
+            suite.append(U)
 
     return ', '.join(map(str, suite))  # Retourner la suite sous forme de chaîne
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
 
 
 
